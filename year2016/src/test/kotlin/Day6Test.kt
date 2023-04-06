0c5c5bfc4b290fb39d18a4da0d07f6e844a33261
Joost Veenstra <Joost.Veenstra@keylane.com>
2023-04-06 11:40:42 +0200

    Day 6: Signals and Noise

---
+++ year2016/src/test/kotlin/Day6Test.kt
@@ -0,0 +1,11 @@
+class Day6Test : DayTest(Day6, {
+    part1 {
+        example = "easter"
+        input = "ikerpcty"
+    }
+
+    part2 {
+        example = "advent"
+        input = ""
+    }
+})
