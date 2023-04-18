f8d69581696605b111a06761e42e3f2e97b68983
Joost Veenstra <Joost.Veenstra@keylane.com>
2023-04-18 13:10:06 +0200

    Day 13: A Maze of Twisty Little Cubicles

---
+++ year2016/src/main/kotlin/Day13.kt
@@ -0,0 +1,57 @@
+object Day13 : Day<Int> {
+    private val start = Point(1, 1)
+    private const val steps = 50
+
+    private fun String.toGoal() = if (toInt() == 10) Point(7, 4) else Point(31, 39)
+
+    private fun Point.isOpenSpace(favorite: Int): Boolean {
+        val number = (x * x) + (3 * x) + (2 * x * y) + y + (y * y) + favorite
+        return number.countOneBits() % 2 == 0
+    }
+
+    private fun Point.isValid() = x >= 0 && y >= 0
+    private fun Point.validNeighbours(favorite: Int) = cardinalNeighbours.filter { it.isValid() }.filter { it.isOpenSpace(favorite) }
+
+    private fun findShortestPathTo(goal: Point, favorite: Int): Int {
+        val queue = dequeOf(start)
+        val visited = mutableMapOf(start to 0)
+
+        while (queue.isNotEmpty()) {
+            val point = queue.removeFirst()
+            if (point == goal) return visited.getValue(point)
+            val cost = visited.getValue(point) + 1
+            point.validNeighbours(favorite).forEach { next ->
+                if (next !in visited || cost < visited.getValue(next)) {
+                    visited[next] = cost
+                    queue.addLast(next)
+                }
+            }
+        }
+
+        error("This should never happen")
+    }
+
+    private fun findNumberOfPossibleGoals(favorite: Int): Int {
+        val queue = dequeOf(start)
+        val visited = mutableMapOf(start to 0)
+
+        while (queue.isNotEmpty()) {
+            val point = queue.removeFirst()
+            val cost = visited.getValue(point) + 1
+            if (cost <= steps) {
+                point.validNeighbours(favorite).forEach { next ->
+                    if (next !in visited || cost < visited.getValue(next)) {
+                        visited[next] = cost
+                        queue.addLast(next)
+                    }
+                }
+            }
+        }
+
+        return visited.size
+    }
+
+    override fun part1(input: String) = findShortestPathTo(input.toGoal(), input.toInt())
+
+    override fun part2(input: String) = findNumberOfPossibleGoals(input.toInt())
+}
---
+++ year2016/src/test/kotlin/Day13Test.kt
@@ -0,0 +1,11 @@
+class Day13Test : DayTest(Day13, {
+    part1 {
+        example = 11
+        input = 92
+    }
+
+    part2 {
+        example = 151
+        input = 124
+    }
+})
