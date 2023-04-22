bd494c0591928e8d88777236d98337e579d022e6
Joost Veenstra <Joost.Veenstra@keylane.com>
2023-04-22 22:29:53 +0200

    Day 21: Scrambled Letters and Hash

---
+++ year2016/src/test/kotlin/Day21Test.kt
@@ -0,0 +1,11 @@
+class Day21Test : DayTest(Day21, {
+    part1 {
+        example = "fbdecgha"
+        input = "baecdfgh"
+    }
+
+    part2 {
+        example = "efghdabc"
+        input = "cegdahbf"
+    }
+})
