871bd2211c45f9daedb168e26bba5eeb8ce67dd7
Joost Veenstra <Joost.Veenstra@keylane.com>
2023-04-26 12:49:13 +0200

    Day 2: Corruption Checksum

---
+++ year2017/src/main/kotlin/Day2.kt
@@ -0,0 +1,11 @@
+object Day2 : Day<Int> {
+    override fun part1(input: String) = input.lines().map { it.allInts() }.sumOf { it.max() - it.min() }
+
+    override fun part2(input: String) = input.lines().map { it.allInts() }.sumOf { row ->
+        row.toList().combinations(2).firstNotNullOf { (a, b) ->
+            val max = maxOf(a, b)
+            val min = minOf(a, b)
+            if (max % min == 0) max / min else null
+        }
+    }
+}
---
+++ year2017/src/test/kotlin/Day2Test.kt
@@ -0,0 +1,11 @@
+class Day2Test : DayTest(Day2, {
+    part1 {
+        example = 18
+        input = 51833
+    }
+
+    part2 {
+        example2 = 9
+        input = 288
+    }
+})
