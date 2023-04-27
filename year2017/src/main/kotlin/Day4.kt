671ad1a0fe153b24c1a0903449192c2bef5f48e6
Joost Veenstra <Joost.Veenstra@keylane.com>
2023-04-27 16:23:19 +0200

    Day 4: High-Entropy Passphrases

---
+++ year2017/src/main/kotlin/Day4.kt
@@ -0,0 +1,11 @@
+object Day4 : Day<Int> {
+    override fun part1(input: String) = input.lines().count { line ->
+        val words = line.split(' ')
+        words.size == words.distinct().size
+    }
+
+    override fun part2(input: String) = input.lines().count { line ->
+        val words = line.split(' ').map { it.asIterable().sorted() }
+        words.size == words.distinct().size
+    }
+}
