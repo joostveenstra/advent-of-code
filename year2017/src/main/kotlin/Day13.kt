86a43daed29da8134750027c4f93af54a54c611f
Joost Veenstra <Joost.Veenstra@keylane.com>
2023-05-10 22:12:35 +0200

    Day 13: Packet Scanners

---
+++ year2017/src/main/kotlin/Day13.kt
@@ -0,0 +1,17 @@
+object Day13 : Day<Int> {
+    private fun String.toFirewall() = lines().map { line ->
+        line.allInts().toList().let { (depth, range) -> depth to range }
+    }
+
+    private fun List<Pair<Int, Int>>.severity(delay: Int) = mapNotNull { (depth, range) ->
+        val caught = (depth + delay) % (2 * (range - 1)) == 0
+        if (caught) depth * range else null
+    }
+
+    override fun part1(input: String) = input.toFirewall().severity(0).sum()
+
+    override fun part2(input: String): Int {
+        val firewall = input.toFirewall()
+        return generateSequence(0) { it + 1 }.indexOfFirst { firewall.severity(it).isEmpty() }
+    }
+}
