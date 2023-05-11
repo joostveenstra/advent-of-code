748576bf14027d9854595c5e721d26151eda676e
Joost Veenstra <Joost.Veenstra@keylane.com>
2023-05-11 22:26:00 +0200

    Day 16: Permutation Promenade

---
+++ year2017/src/main/kotlin/Day16.kt
@@ -0,0 +1,44 @@
+object Day16 : Day<String> {
+    private val spin = "s(\\d+)".toRegex()
+    private val exchange = "x(\\d+)/(\\d+)".toRegex()
+    private val partner = "p(\\w)/(\\w)".toRegex()
+
+    private fun String.toPrograms() = if (length < 14) "abcde" else "abcdefghijklmnop"
+
+    private fun List<String>.dance(initial: String) = fold(initial) { programs, move ->
+        move.match(spin)?.let { (a) ->
+            val x = a.toInt()
+            programs.takeLast(x) + programs.dropLast(x)
+        } ?: move.match(exchange)?.let { (a, b) ->
+            val x = a.toInt()
+            val y = b.toInt()
+            programs.mapIndexed { index, p ->
+                when (index) {
+                    x -> programs[y]
+                    y -> programs[x]
+                    else -> p
+                }
+            }.joinToString("")
+        } ?: move.match(partner)?.let { (a, b) ->
+            val x = programs.indexOf(a)
+            val y = programs.indexOf(b)
+            programs.mapIndexed { index, p ->
+                when (index) {
+                    x -> programs[y]
+                    y -> programs[x]
+                    else -> p
+                }
+            }.joinToString("")
+        } ?: throw IllegalArgumentException("$move is not a valid move")
+    }
+
+    override fun part1(input: String) = input.split(',').dance(input.toPrograms())
+
+    override fun part2(input: String): String {
+        val initial = input.toPrograms()
+        val moves = input.split(',')
+        val dances = generateSequence(initial) { moves.dance(it) }
+        val period = dances.drop(1).indexOf(initial) + 1
+        return dances.drop(1000000000 % period).first()
+    }
+}
---
+++ year2017/src/test/kotlin/Day16Test.kt
@@ -0,0 +1,11 @@
+class Day16Test : DayTest(Day16, {
+    part1 {
+        example = "baedc"
+        input = "pkgnhomelfdibjac"
+    }
+
+    part2 {
+        example = "abcde"
+        input = "pogbjfihclkemadn"
+    }
+})
