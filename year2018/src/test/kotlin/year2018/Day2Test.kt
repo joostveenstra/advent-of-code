659b280a008b98e6903a7a45154a13b0684d5aba
Joost Veenstra <Joost.Veenstra@keylane.com>
2023-05-25 12:29:26 +0200

    Day 2: Inventory Management System

---
+++ year2018/src/test/kotlin/year2018/Day2Test.kt
@@ -0,0 +1,15 @@
+package year2018
+
+import framework.DayTest
+
+class Day2Test : DayTest(Day2, {
+    part1 {
+        example = 12
+        input = 6474
+    }
+
+    part2 {
+        example2 = "fgij"
+        input = "mxhwoglxgeauywfkztndcvjqr"
+    }
+})
