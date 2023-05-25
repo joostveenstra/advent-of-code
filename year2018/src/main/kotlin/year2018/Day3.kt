b30edf2a524792962dcd0f4464ac3a3a40e2c6bf
Joost Veenstra <Joost.Veenstra@keylane.com>
2023-05-25 20:46:16 +0200

    Day 3: No Matter How You Slice It

---
+++ year2018/src/main/kotlin/year2018/Day3.kt
@@ -0,0 +1,24 @@
+package year2018
+
+import framework.Day
+import util.Point
+import util.allInts
+
+object Day3 : Day<Int> {
+    private fun parse(input: String) = input.lineSequence().run {
+        val claims = map { line ->
+            val (_, left, top, width, height) = line.allInts().toList()
+            sequence { for (x in left until left + width) for (y in top until top + height) yield(Point(x, y)) }
+        }
+        val groups = claims.flatten().groupingBy { it }.eachCount()
+        claims to groups
+    }
+
+    override fun part1(input: String) = parse(input).let { (_, groups) ->
+        groups.count { (_, v) -> v > 1 }
+    }
+
+    override fun part2(input: String) = parse(input).let { (claims, groups) ->
+        1 + claims.indexOfFirst { claim -> claim.all { point -> groups[point] == 1 } }
+    }
+}
---
+++ year2018/src/test/kotlin/year2018/Day3Test.kt
@@ -0,0 +1,15 @@
+package year2018
+
+import framework.DayTest
+
+class Day3Test : DayTest(Day3, {
+    part1 {
+        example = 4
+        input = 118858
+    }
+
+    part2 {
+        example = 3
+            input = 1100
+    }
+})
