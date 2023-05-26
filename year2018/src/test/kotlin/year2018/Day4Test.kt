f3f202c2d1b2017dae90cf01d7d5870af4cfc88b
Joost Veenstra <Joost.Veenstra@keylane.com>
2023-05-26 15:35:32 +0200

    Day 4: Repose Record

---
+++ year2018/src/test/kotlin/year2018/Day4Test.kt
@@ -0,0 +1,15 @@
+package year2018
+
+import framework.DayTest
+
+class Day4Test : DayTest(Day4, {
+    part1 {
+        example = 240
+        input = 8421
+    }
+
+    part2 {
+        example = 4455
+        input = 83359
+    }
+})
