52831b54ab5d725e8442c9290a7328a431242f4d
Joost Veenstra <Joost.Veenstra@keylane.com>
2023-05-26 21:41:11 +0200

    Day 5: Alchemical Reduction

---
+++ year2018/src/main/kotlin/year2018/Day5.kt
@@ -0,0 +1,26 @@
+package year2018
+
+import framework.Day
+
+object Day5 : Day<Int> {
+    private infix fun Char.reactsWith(other: Char) = this.lowercaseChar() == other.lowercaseChar() && (this != other)
+
+    private fun String.react(): Int {
+        val left = toMutableList()
+        val right = mutableListOf<Char>()
+
+        while (left.isNotEmpty()) {
+            if (right.isEmpty()) right.add(left.removeLast())
+            else if (left.last() reactsWith right.last()) {
+                left.removeLast()
+                right.removeLast()
+            } else right.add(left.removeLast())
+        }
+
+        return right.size
+    }
+
+    override fun part1(input: String) = input.react()
+
+    override fun part2(input: String) = ('a'..'z').minOf { type -> input.filter { it.lowercaseChar() != type }.react() }
+}
