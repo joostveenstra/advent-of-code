7180e46aa448b7459b7cc72166101790033a3f9d
Joost Veenstra <Joost.Veenstra@keylane.com>
2023-05-29 10:48:49 +0200

    Day 8: Memory Maneuver

---
+++ year2018/src/main/kotlin/year2018/Day8.kt
@@ -0,0 +1,27 @@
+package year2018
+
+import framework.Day
+
+object Day8 : Day<Int> {
+    data class Node(val children: List<Node>, val metadata: List<Int>)
+
+    private fun String.toNumbers() = split(' ').map { it.toInt() }.iterator()
+
+    private fun parse(numbers: Iterator<Int>): Node {
+        val childCount = numbers.next()
+        val metadataCount = numbers.next()
+        val children = (1..childCount).map { parse(numbers) }
+        val metadata = (1..metadataCount).map { numbers.next() }
+        return Node(children, metadata)
+    }
+
+    override fun part1(input: String): Int {
+        fun Node.sum(): Int = metadata.sum() + children.sumOf { it.sum() }
+        return parse(input.toNumbers()).sum()
+    }
+
+    override fun part2(input: String): Int {
+        fun Node.sum(): Int = if (children.isEmpty()) metadata.sum() else metadata.mapNotNull { children.getOrNull(it - 1)?.sum() }.sum()
+        return parse(input.toNumbers()).sum()
+    }
+}
---
+++ year2018/src/test/kotlin/year2018/Day8Test.kt
@@ -0,0 +1,15 @@
+package year2018
+
+import framework.DayTest
+
+class Day8Test : DayTest(Day8, {
+    part1 {
+        example = 138
+        input = 42501
+    }
+
+    part2 {
+        example = 66
+        input = 30857
+    }
+})
