89a74c45dab6aadbaddb121530d60504a95ab34d
Joost Veenstra <Joost.Veenstra@keylane.com>
2023-05-30 13:40:54 +0200

    Day 9: Marble Mania

---
+++ year2018/src/test/kotlin/year2018/Day9Test.kt
@@ -0,0 +1,15 @@
+package year2018
+
+import framework.DayTest
+
+class Day9Test : DayTest(Day9, {
+    part1 {
+        example = 37305L
+        input = 382055L
+    }
+
+    part2 {
+        example = 320997431L
+        input = 3133277384L
+    }
+})
