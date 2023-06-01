7a8e0c8fe10c3b6d1093ca4ff0ab0455f136a69c
Joost Veenstra <Joost.Veenstra@keylane.com>
2023-06-01 12:35:58 +0200

    Day 11: Chronal Charge

---
+++ year2018/src/main/kotlin/year2018/Day11.kt
@@ -0,0 +1,43 @@
+package year2018
+
+import framework.Day
+
+object Day11 : Day<Any> {
+    data class Square(val x: Int, val y: Int, val size: Int, val sum: Int)
+
+    private fun Int.toFuelCells() = Array(301) { x ->
+        IntArray(301) { y ->
+            ((x + 10) * y + this) * (x + 10) / 100 % 10 - 5
+        }
+    }
+
+    private fun Array<IntArray>.toSummedAreaTable() = apply {
+        for (x in 1..300) for (y in 1..300) {
+            this[x][y] = this[x][y] + this[x - 1][y] + this[x][y - 1] - this[x - 1][y - 1]
+        }
+    }
+
+    private fun Array<IntArray>.toSquares(size: Int) = run {
+        val sat = this
+        sequence {
+            for (x in size..300) for (y in size..300) {
+                val sum = sat[x][y] - sat[x - size][y] - sat[x][y - size] + sat[x - size][y - size]
+                yield(Square(x, y, size, sum))
+            }
+        }
+    }
+
+    override fun part1(input: String): String {
+        val sat = input.toInt().toFuelCells().toSummedAreaTable()
+        val squares = sat.toSquares(3)
+        val (x, y, size) = squares.maxBy { it.sum }
+        return listOf(x - size + 1, y - size + 1).joinToString(",")
+    }
+
+    override fun part2(input: String): String {
+        val sat = input.toInt().toFuelCells().toSummedAreaTable()
+        val squares = (1..300).asSequence().flatMap { size -> sat.toSquares(size) }
+        val (x, y, size) = squares.maxBy { it.sum }
+        return listOf(x - size + 1, y - size + 1, size).joinToString(",")
+    }
+}
---
+++ year2018/src/test/kotlin/year2018/Day11Test.kt
@@ -0,0 +1,15 @@
+package year2018
+
+import framework.DayTest
+
+class Day11Test : DayTest(Day11, {
+    part1 {
+        example = "33,45"
+        input = "20,68"
+    }
+
+    part2 {
+        example = "90,269,16"
+        input = "231,273,16"
+    }
+})
