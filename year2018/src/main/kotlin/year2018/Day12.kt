b46ff1d88042672c26732572fcc74ca096a4b497
Joost Veenstra <Joost.Veenstra@keylane.com>
2023-06-05 11:27:45 +0200

    Day 12: Subterranean Sustainability

---
+++ year2018/src/main/kotlin/year2018/Day12.kt
@@ -0,0 +1,36 @@
+package year2018
+
+import framework.Day
+
+object Day12 : Day<Any> {
+    private fun String.toPlantsRules() = lines().run {
+        val plants = first().asSequence().drop(15).withIndex().filter { it.value == '#' }.map { it.index }.toSet()
+        val rules = drop(2).filter { it.last() == '#' }.map { it.take(5) }.toSet()
+        plants to rules
+    }
+
+    private fun Set<Int>.step(rules: Set<String>) = (min() - 2..max() + 2).mapNotNull { index ->
+        val pattern = (-2..2).fold("") { pattern, offset ->
+            pattern + if ((index + offset) in this) '#' else '.'
+        }
+        if (pattern in rules) index else null
+    }.toSet()
+
+    override fun part1(input: String): Int {
+        val (plants, rules) = input.toPlantsRules()
+        return generateSequence(plants) { it.step(rules) }.drop(20).first().sum()
+    }
+
+    override fun part2(input: String): Long {
+        val (plants, rules) = input.toPlantsRules()
+
+        tailrec fun Set<Int>.lastSum(previousPreviousDelta: Int, previousDelta: Int, generation: Int): Long {
+            val next = step(rules)
+            val delta = next.sum() - sum()
+            return if (delta == previousDelta && delta == previousPreviousDelta) sum() + delta * (50000000000L - generation)
+            else next.lastSum(previousDelta, delta, generation + 1)
+        }
+
+        return plants.lastSum(-1, 0, 0)
+    }
+}
---
+++ year2018/src/test/kotlin/year2018/Day12Test.kt
@@ -0,0 +1,15 @@
+package year2018
+
+import framework.DayTest
+
+class Day12Test : DayTest(Day12, {
+    part1 {
+        example = 325
+        input = 1991
+    }
+
+    part2 {
+        example = 999999999374L
+        input = 1100000000511L
+    }
+})
