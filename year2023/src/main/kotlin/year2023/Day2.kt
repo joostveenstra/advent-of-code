86dcef7c0be1d6e654eba7c55908c1085bc72f85
Joost Veenstra <Joost.Veenstra@keylane.com>
2023-12-02 10:29:35 +0100

    Day 2: Cube Conundrum

---
+++ year2023/src/main/kotlin/year2023/Day2.kt
@@ -0,0 +1,39 @@
+package year2023
+
+import framework.Day
+
+object Day2 : Day<Int> {
+    private val max = mapOf("red" to 12, "green" to 13, "blue" to 14)
+
+    override fun part1(input: String) = input.lines()
+        .map { line ->
+            val (game, remaining) = line.split(": ")
+            val id = game.split(" ").last().toInt()
+            val sets = remaining.split("; ")
+            val isPossible = sets.all { set ->
+                val cubes = set.split(", ")
+                cubes.all { cube ->
+                    val (amount, color) = cube.split(" ")
+                    amount.toInt() <= max.getValue(color)
+                }
+            }
+            id to isPossible
+        }
+        .filter { it.second }
+        .sumOf { it.first }
+
+    override fun part2(input: String) = input.lines()
+        .sumOf { line ->
+            val (_, remaining) = line.split(": ")
+            val sets = remaining.split("; ")
+            val count = mutableMapOf<String, Int>()
+            sets.forEach { set ->
+                val cubes = set.split(", ")
+                cubes.forEach { cube ->
+                    val (amount, color) = cube.split(" ")
+                    count[color] = maxOf(count.getOrDefault(color, 0), amount.toInt())
+                }
+            }
+            count.values.reduce { a, b -> a * b }
+        }
+}
