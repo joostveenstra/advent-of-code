3737e8ecb8510a912816a071a6ff26feeb8cc88b
Joost Veenstra <Joost.Veenstra@keylane.com>
2023-12-06 11:10:07 +0100

    Day 5: If You Give A Seed A Fertilizer

--- util/src/main/kotlin/util/Regex.kt
+++ util/src/main/kotlin/util/Regex.kt
@@ -3,6 +3,7 @@ package util
 val EMPTY_LINE = "\r?\n\r?\n".toRegex()
 
 fun String.allInts() = "-?\\d+".toRegex().findAll(this).map { it.value.toInt() }
+fun String.allLongs() = "-?\\d+".toRegex().findAll(this).map { it.value.toLong() }
 
 fun String.match(regex: Regex) = regex.matchEntire(this)?.destructured
 
---
+++ year2023/src/test/kotlin/year2023/Day5Test.kt
@@ -0,0 +1,15 @@
+package year2023
+
+import framework.DayTest
+
+class Day5Test : DayTest(Day5, {
+    part1 {
+        example = 35L
+        input = 282277027L
+    }
+
+    part2 {
+        example = 46L
+        input = 11554135L
+    }
+})
