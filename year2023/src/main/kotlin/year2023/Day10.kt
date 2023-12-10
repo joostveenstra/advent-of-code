d410432a2505fa820b3c91fd339f8ff730e1d812
Joost Veenstra <Joost.Veenstra@keylane.com>
2023-12-10 21:06:05 +0100

    Day 10: Pipe Maze

---
+++ year2023/src/main/kotlin/year2023/Day10.kt
@@ -0,0 +1,43 @@
+package year2023
+
+import framework.Day
+import util.*
+import kotlin.math.absoluteValue
+
+/**
+ * Uses https://en.wikipedia.org/wiki/Shoelace_formula to calculate the area enclosed by the loop
+ * and https://en.wikipedia.org/wiki/Pick%27s_theorem to calculate the amount of points inside that area
+ */
+object Day10 : Day<Int> {
+    private fun String.toTiles() = buildMap {
+        lines().forEachIndexed { y, row ->
+            row.forEachIndexed { x, char -> put(Point(x, y), char) }
+        }
+    }
+
+    private fun determinant(a: Point, b: Point) = a.x * b.y - a.y * b.x
+
+    private fun Map<Point, Char>.walk(): Pair<Int, Int> {
+        tailrec fun step(position: Point, direction: Point, area: Int, steps: Int): Pair<Int, Int> {
+            val nextPosition = position + direction
+            val determinant = determinant(position, nextPosition)
+            val nextDirection = when (get(nextPosition)) {
+                '|', '-' -> direction
+                'L' -> if (direction == SOUTH) EAST else NORTH
+                'J' -> if (direction == SOUTH) WEST else NORTH
+                '7' -> if (direction == NORTH) WEST else SOUTH
+                'F' -> if (direction == NORTH) EAST else SOUTH
+                else -> return steps to (area + determinant).absoluteValue / 2
+            }
+            return step(nextPosition, nextDirection, area + determinant, steps + 1)
+        }
+
+        val start = entries.first { it.value == 'S' }.key
+        val direction = if (get(start + NORTH) in listOf('|', '7', 'F')) NORTH else SOUTH
+        return step(start, direction, 0, 1)
+    }
+
+    override fun part1(input: String) = input.toTiles().walk().let { (steps) -> steps / 2 }
+
+    override fun part2(input: String) = input.toTiles().walk().let { (steps, area) -> area - steps / 2 + 1 }
+}
---
+++ year2023/src/test/kotlin/year2023/Day10Test.kt
@@ -0,0 +1,15 @@
+package year2023
+
+import framework.DayTest
+
+class Day10Test : DayTest(Day10, {
+    part1 {
+        example = 8
+        input = 6956
+    }
+
+    part2 {
+        example2 = 10
+        input = 455
+    }
+})
