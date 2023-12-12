be1521d786cec87aa57ca26ca0f627951f936471
Joost Veenstra <Joost.Veenstra@keylane.com>
2023-12-12 18:52:11 +0100

    Day 12: Hot Springs

--- year2023/src/main/kotlin/year2023/Day12.kt
+++ year2023/src/main/kotlin/year2023/Day12.kt
@@ -11,19 +11,19 @@ object Day12 : Day<Long> {
         }
     }
 
-    private fun List<Pair<String, List<Int>>>.numberOfSolutions() = sumOf { (springs, sizes) ->
+    private fun Pair<String, List<Int>>.arrangements() = let { (springs, sizes) ->
         val cache = mutableMapOf<Triple<String, List<Int>, Int>, Long>()
-        fun numberOfSolutions(springs: String, sizes: List<Int>, done: Int = 0): Long = cache.getOrPut(Triple(springs, sizes, done)) {
+        fun arrangements(springs: String, sizes: List<Int>, done: Int = 0): Long = cache.getOrPut(Triple(springs, sizes, done)) {
             when {
                 springs.isEmpty() -> if (sizes.isEmpty() && done == 0) 1 else 0
                 else -> {
                     val candidates = springs.first().let { if (it == '?') listOf('.', '#') else listOf(it) }
                     candidates.sumOf { spring ->
                         when (spring) {
-                            '#' -> numberOfSolutions(springs.drop(1), sizes, done + 1)
+                            '#' -> arrangements(springs.drop(1), sizes, done + 1)
                             else -> when (done) {
-                                0 -> numberOfSolutions(springs.drop(1), sizes)
-                                sizes.firstOrNull() -> numberOfSolutions(springs.drop(1), sizes.drop(1))
+                                0 -> arrangements(springs.drop(1), sizes)
+                                sizes.firstOrNull() -> arrangements(springs.drop(1), sizes.drop(1))
                                 else -> 0
                             }
                         }
@@ -31,10 +31,10 @@ object Day12 : Day<Long> {
                 }
             }
         }
-        numberOfSolutions("$springs.", sizes)
+        arrangements("$springs.", sizes)
     }
 
-    override fun part1(input: String) = input.toRows(1).numberOfSolutions()
+    override fun part1(input: String) = input.toRows(1).sumOf { it.arrangements() }
 
-    override fun part2(input: String) = input.toRows(5).numberOfSolutions()
+    override fun part2(input: String) = input.toRows(5).sumOf { it.arrangements() }
 }
