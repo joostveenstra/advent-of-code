5791a796aaca2a11d0f8279ea086fce5c7470f0e
Joost Veenstra <Joost.Veenstra@keylane.com>
2024-01-10 08:23:45 +0100

    Simplify

--- year2023/src/main/kotlin/year2023/Day9.kt
+++ year2023/src/main/kotlin/year2023/Day9.kt
@@ -6,13 +6,11 @@ import util.allInts
 object Day9 : Day<Int> {
     private fun String.toReport() = lines().map { it.allInts().toList() }
 
-    private fun List<Int>.differences() = generateSequence(this) { it.zipWithNext { a, b -> b - a } }.takeWhile { step -> step.any { it != 0 } }
+    private fun List<Int>.extrapolate() = generateSequence(this) { it.zipWithNext { a, b -> b - a } }
+        .takeWhile { step -> step.any { it != 0 } }
+        .sumOf { it.last() }
 
-    override fun part1(input: String) = input.toReport().sumOf { line ->
-        line.differences().map { it.last() }.sum()
-    }
+    override fun part1(input: String) = input.toReport().sumOf { line -> line.extrapolate() }
 
-    override fun part2(input: String) = input.toReport().sumOf { line ->
-        line.differences().map { it.first() }.toList().reversed().reduce { a, b -> b - a }
-    }
+    override fun part2(input: String) = input.toReport().sumOf { line -> line.reversed().extrapolate() }
 }
