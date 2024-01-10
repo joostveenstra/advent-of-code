ce6741899a4754bc7c576677660ea12895ca4e3d
Joost Veenstra <Joost.Veenstra@keylane.com>
2024-01-10 08:14:18 +0100

    Simplify

--- year2023/src/main/kotlin/year2023/Day8.kt
+++ year2023/src/main/kotlin/year2023/Day8.kt
@@ -14,10 +14,10 @@ object Day8 : Day<Long> {
                 node to Node(left, right)
             }
         }
-        return moves to nodes
+        moves to nodes
     }
 
-    private fun stepsToEscape(moves: String, nodes: Map<String, Node>, start: String, isEnd: (String) -> Boolean) =
+    private fun stepsToEscape(moves: String, nodes: Map<String, Node>, start: String) =
         generateSequence(start to 0) { (current, steps) ->
             val move = moves[steps % moves.length]
             val node = nodes.getValue(current)
@@ -26,19 +26,19 @@ object Day8 : Day<Long> {
                 else -> node.right
             }
             next to steps + 1
-        }.dropWhile { (current) -> !isEnd(current) }.first().let { (_, steps) -> steps }.toLong()
+        }.dropWhile { (current) -> !current.endsWith('Z') }.first().let { (_, steps) -> steps }.toLong()
 
 
     override fun part1(input: String): Long {
         val (moves, nodes) = parse(input)
-        return stepsToEscape(moves, nodes, "AAA") { it == "ZZZ" }
+        return stepsToEscape(moves, nodes, "AAA")
     }
 
     override fun part2(input: String): Long {
         val (moves, nodes) = parse(input)
         val startNodes = nodes.keys.filter { it.endsWith('A') }
         return startNodes
-            .map { start -> stepsToEscape(moves, nodes, start) { it.endsWith('Z') } }
+            .map { start -> stepsToEscape(moves, nodes, start) }
             .reduce(::lcm)
     }
 }
