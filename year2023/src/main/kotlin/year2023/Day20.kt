346e9cbb723b48881c9831750fa874a5a286935a
Joost Veenstra <Joost.Veenstra@keylane.com>
2024-01-13 21:44:08 +0100

    Day 20: Pulse Propagation

---
+++ year2023/src/main/kotlin/year2023/Day20.kt
@@ -0,0 +1,84 @@
+package year2023
+
+import framework.Day
+import util.lcm
+
+object Day20 : Day<Long> {
+    sealed interface Module {
+        val name: String
+        val outputs: List<String>
+        fun receive(source: String, pulse: Boolean): List<State> = send(pulse)
+        fun send(pulse: Boolean): List<State> = outputs.map { State(name, it, pulse) }
+    }
+
+    class Broadcaster(override val name: String, override val outputs: List<String>) : Module
+
+    class FlipFlop(override val name: String, override val outputs: List<String>, private var on: Boolean = false) : Module {
+        override fun receive(source: String, pulse: Boolean) = if (pulse) listOf() else apply { on = !on }.send(on)
+    }
+
+    class Conjunction(override val name: String, override val outputs: List<String>, val memory: MutableMap<String, Boolean>) : Module {
+        override fun receive(source: String, pulse: Boolean): List<State> = apply { memory[source] = pulse }.send(!memory.all { it.value })
+    }
+
+    data class State(val source: String, val destination: String, val pulse: Boolean)
+
+    private fun String.toModules() = lines().map { line ->
+        line.split(" -> ").let { (a, b) ->
+            val name = a.drop(1)
+            val outputs = b.split(", ")
+            when (a.first()) {
+                '%' -> FlipFlop(name, outputs)
+                '&' -> Conjunction(name, outputs, mutableMapOf())
+                else -> Broadcaster(a, outputs)
+            }
+        }
+    }.also { modules ->
+        modules.filterIsInstance<Conjunction>().forEach { conjunction ->
+            modules.filter { conjunction.name in it.outputs }.forEach { conjunction.memory[it.name] = false }
+        }
+    }.associateBy { it.name }
+
+    private fun pressButton(
+        modules: Map<String, Module>,
+        parents: Set<String> = setOf(),
+        pressed: Long = 0L,
+        cycles: Map<String, Long> = mapOf()
+    ): Triple<Long, Long, Map<String, Long>> {
+        fun State.next(): List<State> = modules[destination]?.receive(source, pulse) ?: listOf()
+        tailrec fun List<State>.propagate(low: Long, high: Long, cycles: Map<String, Long>): Triple<Long, Long, Map<String, Long>> =
+            if (isEmpty()) Triple(low, high, cycles)
+            else {
+                val next = flatMap { it.next() }
+                val nextCycles = next.filter { !it.pulse && it.destination in parents }.fold(cycles) { acc, (_, destination) ->
+                    acc + (destination to (acc[destination]?.let { pressed - it } ?: pressed))
+                }
+                next.propagate(low + next.count { it.pulse.not() }, high + next.count { it.pulse }, nextCycles)
+            }
+
+        val initial = State("button", "broadcaster", false)
+        return listOf(initial).propagate(1, 0, cycles)
+    }
+
+    override fun part1(input: String): Long {
+        val modules = input.toModules()
+        val (low, high) = (1..1000).fold(0L to 0L) { (low, high), _ ->
+            pressButton(modules).let { (l, h) -> low + l to high + h }
+        }
+        return low * high
+    }
+
+    override fun part2(input: String): Long {
+        val modules = input.toModules()
+        val parents = modules.filterValues { parent -> modules.filterValues { "rx" in it.outputs }.keys.any { it in parent.outputs } }.keys
+
+        tailrec fun minimize(pressed: Long = 1L, cycles: Map<String, Long> = mapOf()): Long =
+            if (cycles.size == parents.size) cycles.values.reduce(Long::lcm)
+            else {
+                val (_, _, nextCycles) = pressButton(modules, parents, pressed, cycles)
+                minimize(pressed + 1, nextCycles)
+            }
+
+        return minimize()
+    }
+}
---
+++ year2023/src/test/kotlin/year2023/Day20Test.kt
@@ -0,0 +1,14 @@
+package year2023
+
+import framework.DayTest
+
+class Day20Test : DayTest(Day20, {
+    part1 {
+        example = 11687500L
+        input = 883726240L
+    }
+
+    part2 {
+        input = 211712400442661L
+    }
+})
