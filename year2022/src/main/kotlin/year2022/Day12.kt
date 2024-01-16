d1f754fa3d42eeee8ab55798c46d3b16010848e3
Joost Veenstra <Joost.Veenstra@keylane.com>
2024-01-16 11:16:56 +0100

    Introduce Grid

--- year2022/src/main/kotlin/year2022/Day12.kt
+++ year2022/src/main/kotlin/year2022/Day12.kt
@@ -1,36 +1,27 @@
 package year2022
 
 import framework.Day
-import util.Point
+import util.Grid
 import util.dequeOf
-
-typealias Grid = Map<Point, Char>
+import util.toCharGrid
 
 object Day12 : Day<Int> {
-    private fun String.toGrid(): Grid = buildMap {
-        lines().forEachIndexed { y, row ->
-            row.forEachIndexed { x, char ->
-                put(Point(x, y), char)
-            }
-        }
-    }
-
     private fun Char.height() = when (this) {
         'S' -> 'a'
         'E' -> 'z'
         else -> this
     }
 
-    private fun Grid.shortestPathTo(goal: Char): Int {
-        val start = entries.first { it.value == 'E' }.key
+    private fun Grid<Char>.shortestPathTo(goal: Char): Int {
+        val start = find('E')
         val queue = dequeOf(start)
         val visited = mutableMapOf(start to 0)
 
         while (queue.isNotEmpty()) {
             val point = queue.removeFirst()
-            if (getValue(point) == goal) return visited.getValue(point)
+            if (get(point) == goal) return visited.getValue(point)
             point.cardinalNeighbours.filter { it in this }.filterNot { it in visited }.forEach { next ->
-                if (getValue(point).height() - getValue(next).height() <= 1) {
+                if (get(point).height() - get(next).height() <= 1) {
                     queue.addLast(next)
                     visited[next] = visited.getValue(point) + 1
                 }
@@ -40,7 +31,7 @@ object Day12 : Day<Int> {
         error("This should never happen")
     }
 
-    override fun part1(input: String) = input.toGrid().shortestPathTo('S')
+    override fun part1(input: String) = input.toCharGrid().shortestPathTo('S')
 
-    override fun part2(input: String) = input.toGrid().shortestPathTo('a')
+    override fun part2(input: String) = input.toCharGrid().shortestPathTo('a')
 }
--- year2022/src/main/kotlin/year2022/Day8.kt
+++ year2022/src/main/kotlin/year2022/Day8.kt
@@ -1,37 +1,27 @@
 package year2022
 
 import framework.Day
-import util.Point
-import util.cardinal
-import util.productOf
+import util.*
 
 object Day8 : Day<Int> {
-    private fun String.toGrid() = buildMap {
-        lines().forEachIndexed { y, row ->
-            row.forEachIndexed { x, char ->
-                put(Point(x, y), char.digitToInt())
-            }
-        }
-    }
-
-    private fun Point.canLookOutside(grid: Map<Point, Int>, direction: Point, height: Int): Boolean {
+    private fun Point.canLookOutside(grid: Grid<Int>, direction: Point, height: Int): Boolean {
         val next = this + direction
-        return grid[next]?.let { if (it < height) next.canLookOutside(grid, direction, height) else false } ?: true
+        return grid.safeGet(next)?.let { if (it < height) next.canLookOutside(grid, direction, height) else false } ?: true
     }
 
-    private fun Point.viewingDistance(grid: Map<Point, Int>, direction: Point, height: Int): Int {
+    private fun Point.viewingDistance(grid: Grid<Int>, direction: Point, height: Int): Int {
         val next = this + direction
-        return grid[next]?.let { if (it < height) 1 + next.viewingDistance(grid, direction, height) else 1 } ?: 0
+        return grid.safeGet(next)?.let { if (it < height) 1 + next.viewingDistance(grid, direction, height) else 1 } ?: 0
     }
 
-    override fun part1(input: String) = input.toGrid().let { grid ->
-        grid.count { (tree, height) ->
+    override fun part1(input: String) = input.toIntGrid().let { grid ->
+        grid.entries.count { (tree, height) ->
             cardinal.any { direction -> tree.canLookOutside(grid, direction, height) }
         }
     }
 
-    override fun part2(input: String) = input.toGrid().let { grid ->
-        grid.maxOf { (tree, height) ->
+    override fun part2(input: String) = input.toIntGrid().let { grid ->
+        grid.entries.maxOf { (tree, height) ->
             cardinal.productOf { direction -> tree.viewingDistance(grid, direction, height) }
         }
     }
