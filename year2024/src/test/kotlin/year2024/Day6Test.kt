e3e86b45b83a7576b5f9ca80cf5b3ebf6f0656a2
Joost Veenstra <Joost.Veenstra@keylane.com>
2025-02-10 19:06:09 +0100

    Day 6: Guard Gallivant

---
+++ year2024/src/test/kotlin/year2024/Day6Test.kt
@@ -0,0 +1,15 @@
+package year2024
+
+import framework.DayTest
+
+class Day6Test : DayTest(Day6, {
+    part1 {
+        example = 41
+        input = 5312
+    }
+
+    part2 {
+        example = 6
+        input = 1748
+    }
+})
