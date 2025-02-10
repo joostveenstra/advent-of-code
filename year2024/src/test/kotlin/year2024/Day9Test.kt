c8dd2b675b6f4ce849a64ba65ec4f45b67dd190b
Joost Veenstra <Joost.Veenstra@keylane.com>
2025-02-10 19:06:59 +0100

    Day 9: Disk Fragmenter

---
+++ year2024/src/test/kotlin/year2024/Day9Test.kt
@@ -0,0 +1,15 @@
+package year2024
+
+import framework.DayTest
+
+class Day9Test : DayTest(Day9, {
+    part1 {
+        example = 1928
+        input = 6258319840548
+    }
+
+    part2 {
+        example = 2858
+        input = 6286182965311
+    }
+})
