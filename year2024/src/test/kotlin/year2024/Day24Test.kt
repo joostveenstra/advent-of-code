4c4083711bb821e0f2b8cc207a5ba308c858d0b2
Joost Veenstra <Joost.Veenstra@keylane.com>
2025-02-10 19:10:37 +0100

    Day 24: Crossed Wires

---
+++ year2024/src/test/kotlin/year2024/Day24Test.kt
@@ -0,0 +1,14 @@
+package year2024
+
+import framework.DayTest
+
+class Day24Test : DayTest(Day24, {
+    part1 {
+        example = 2024
+        input = 60714423975686
+    }
+
+    part2 {
+        input = "cgh,frt,pmd,sps,tst,z05,z11,z23"
+    }
+})
