5dd06a3fb62d5dff92c3154652fcc438bd3414ab
Joost Veenstra <Joost.Veenstra@keylane.com>
2025-02-10 19:06:25 +0100

    Day 7: Bridge Repair

---
+++ year2024/src/test/kotlin/year2024/Day7Test.kt
@@ -0,0 +1,15 @@
+package year2024
+
+import framework.DayTest
+
+class Day7Test : DayTest(Day7, {
+    part1 {
+        example = 3749
+        input = 5540634308362
+    }
+
+    part2 {
+        example = 11387
+        input = 472290821152397
+    }
+})
