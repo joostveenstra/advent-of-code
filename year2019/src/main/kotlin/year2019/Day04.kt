c60dadcd5d8f3c4be15b5304a18422be5b847b33
Joost Veenstra <Joost.Veenstra@keylane.com>
2025-11-26 22:28:32 +0100

    Day 4: Secure Container

---
+++ year2019/src/main/kotlin/year2019/Day04.kt
@@ -0,0 +1,15 @@
+package year2019
+
+import framework.Context
+import framework.Day
+
+class Day04(context: Context) : Day by context {
+    val range = input.split('-').let { (min, max) -> min.toInt()..max.toInt() }.asSequence().map { it.toString() }
+
+    fun String.increasing() = zipWithNext().all { (a, b) -> a <= b }
+    fun String.adjacent() = zipWithNext().any { (a, b) -> a == b }
+    fun String.strictlyAdjacent() = groupBy { it }.any { it.value.size == 2 }
+
+    fun part1() = range.count { it.increasing() && it.adjacent() }
+    fun part2() = range.count { it.increasing() && it.strictlyAdjacent() }
+}
---
+++ year2019/src/test/kotlin/year2019/Day04Test.kt
@@ -0,0 +1,13 @@
+package year2019
+
+import framework.Test
+
+class Day04Test : Test({
+    part1 {
+        input = 945
+    }
+
+    part2 {
+        input = 617
+    }
+})
