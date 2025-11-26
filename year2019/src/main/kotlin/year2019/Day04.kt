6392603fb9dc0ec038acb10418aa33c27022a716
Joost Veenstra <Joost.Veenstra@keylane.com>
2025-11-26 22:33:26 +0100

    Day 4: Secure Container

--- year2019/src/main/kotlin/year2019/Day04.kt
+++ year2019/src/main/kotlin/year2019/Day04.kt
@@ -2,13 +2,14 @@ package year2019
 
 import framework.Context
 import framework.Day
+import util.frequencies
 
 class Day04(context: Context) : Day by context {
     val range = input.split('-').let { (min, max) -> min.toInt()..max.toInt() }.asSequence().map { it.toString() }
 
     fun String.increasing() = zipWithNext().all { (a, b) -> a <= b }
     fun String.adjacent() = zipWithNext().any { (a, b) -> a == b }
-    fun String.strictlyAdjacent() = groupBy { it }.any { it.value.size == 2 }
+    fun String.strictlyAdjacent() = frequencies().any { it.value == 2 }
 
     fun part1() = range.count { it.increasing() && it.adjacent() }
     fun part2() = range.count { it.increasing() && it.strictlyAdjacent() }
