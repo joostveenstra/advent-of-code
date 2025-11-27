ae524ca973d65aa7e38d9168cb7b432e11a0e3d7
Joost Veenstra <Joost.Veenstra@keylane.com>
2025-11-27 20:18:33 +0100

    Day 1: Report Repair

---
+++ year2020/src/main/kotlin/year2020/Day01.kt
@@ -0,0 +1,14 @@
+package year2020
+
+import framework.Context
+import framework.Day
+import util.combinations
+import util.product
+
+class Day01(context: Context) : Day by context {
+    val numbers = lines.map { it.toInt() }
+    val reverse = numbers.map { 2020 - it }
+
+    fun part1() = numbers.first { it in reverse }.let { it * (2020 - it) }
+    fun part2() = numbers.combinations(2).first { 2020 - it.sum() in numbers }.let { (2020 - it.sum()) * it.product() }
+}
---
+++ year2020/src/test/kotlin/year2020/Day01Test.kt
@@ -0,0 +1,15 @@
+package year2020
+
+import framework.Test
+
+class Day01Test : Test({
+    part1 {
+        example = 514579
+        input = 927684
+    }
+
+    part2 {
+        example = 241861950
+        input = 292093004
+    }
+})
