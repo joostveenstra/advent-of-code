7034845d00d19a90291c085e57f69b72a57be42c
Joost Veenstra <Joost.Veenstra@keylane.com>
2025-11-28 07:57:09 +0100

    Speedup

--- year2019/src/main/kotlin/year2019/Day04.kt
+++ year2019/src/main/kotlin/year2019/Day04.kt
@@ -5,12 +5,14 @@ import framework.Day
 import util.frequencies
 
 class Day04(context: Context) : Day by context {
-    val range = input.split('-').let { (min, max) -> min.toInt()..max.toInt() }.asSequence().map { it.toString() }
+    val range = input.split('-').let { (min, max) -> min.toInt()..max.toInt() }.map { it.toString() }
 
     fun String.increasing() = zipWithNext().all { (a, b) -> a <= b }
     fun String.adjacent() = zipWithNext().any { (a, b) -> a == b }
     fun String.strictlyAdjacent() = frequencies().any { it.value == 2 }
+    
+    val increasing = range.filter { it.increasing() }
 
-    fun part1() = range.count { it.increasing() && it.adjacent() }
-    fun part2() = range.count { it.increasing() && it.strictlyAdjacent() }
+    fun part1() = increasing.count { it.adjacent() }
+    fun part2() = increasing.count { it.strictlyAdjacent() }
 }
