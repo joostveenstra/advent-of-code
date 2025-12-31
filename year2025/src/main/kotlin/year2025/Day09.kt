b78200a666e7419e124d504aedcb01b0d9fd46fa
Joost Veenstra <Joost.Veenstra@keylane.com>
2025-12-31 11:38:00 +0100

    Day 9: Movie Theater

---
+++ year2025/src/main/kotlin/year2025/Day09.kt
@@ -0,0 +1,32 @@
+package year2025
+
+import framework.Context
+import framework.Day
+import util.*
+import kotlin.math.max
+import kotlin.math.min
+
+class Day09(context: Context) : Day by context {
+    data class Rectangle(val x: IntRange, val y: IntRange) {
+        constructor(a: Point, b: Point) : this(
+            min(a.x, b.x)..max(a.x, b.x),
+            min(a.y, b.y)..max(a.y, b.y)
+        )
+
+        val area = x.width.toLong() * y.width
+        
+        fun inner() = Rectangle(
+            x.first + 1..<x.last,
+            y.first + 1..<y.last
+        )
+
+        infix fun overlaps(other: Rectangle) = x overlaps other.x && y overlaps other.y
+    }
+
+    val tiles = lines.map { it.toPoint() }
+    val edges = (tiles + tiles.first()).zipWithNext { a, b -> Rectangle(a, b) }
+    val rectangles = tiles.pairs().map { (a, b) -> Rectangle(a, b) }.sortedByDescending { it.area }
+
+    fun part1() = rectangles.first().area
+    fun part2() = rectangles.first { r -> edges.none { r.inner() overlaps it } }.area
+}
---
+++ year2025/src/test/kotlin/year2025/Day09Test.kt
@@ -0,0 +1,15 @@
+package year2025
+
+import framework.Test
+
+class Day09Test : Test({
+    part1 {
+        example = 50
+        input = 4756718172
+    }
+
+    part2 {
+        example = 24
+        input = 1665679194
+    }
+})
