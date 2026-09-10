fdf8c403dc2d9fe588c8d0c10f9cc1e20b4ee88d
Joost Veenstra <Joost.Veenstra@keylane.com>
2026-09-10 10:38:42 +0200

    Day 4: Passport Processing

---
+++ year2020/src/main/kotlin/year2020/Day04.kt
@@ -0,0 +1,33 @@
+package year2020
+
+import framework.Context
+import framework.Day
+import util.EMPTY_LINE
+
+class Day04(context: Context) : Day by context {
+    val passports = input.split(EMPTY_LINE).map { p ->
+        p.split(":|\\s+".toRegex()).chunked(2).filter { (key) -> key != "cid" }
+    }
+
+    fun String.inRange(r: IntRange) = toInt() in r
+
+    fun validateField(key: String, value: String) = when (key) {
+        "byr" -> value.inRange(1920..2002)
+        "iyr" -> value.inRange(2010..2020)
+        "eyr" -> value.inRange(2020..2030)
+        "hgt" -> value.isValidHeight()
+        "hcl" -> value.matches("#[0-9a-f]{6}".toRegex())
+        "ecl" -> value in setOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
+        "pid" -> value.length == 9 && value.all { it.isDigit() }
+        else -> false
+    }
+
+    fun String.isValidHeight() = when (takeLast(2)) {
+        "cm" -> dropLast(2).inRange(150..193)
+        "in" -> dropLast(2).inRange(59..76)
+        else -> false
+    }
+
+    fun part1() = passports.count { it.size == 7 }
+    fun part2() = passports.count { it.size == 7 && it.all { (key, value) -> validateField(key, value) } }
+}
---
+++ year2020/src/test/kotlin/year2020/Day04Test.kt
@@ -0,0 +1,15 @@
+package year2020
+
+import framework.Test
+
+class Day04Test : Test({
+    part1 {
+        example = 2
+        input = 264
+    }
+
+    part2 {
+        example = 2
+        input = 224
+    }
+})
