6628f3e329ba7301cd4ed25f0913d820876f6ac6
Joost Veenstra <Joost.Veenstra@keylane.com>
2026-09-18 19:47:16 +0200

    Day 4: Giant Squid

---
+++ year2021/src/main/kotlin/year2021/Day04.kt
@@ -0,0 +1,25 @@
+package year2021
+
+import framework.Context
+import framework.Day
+import util.*
+
+class Day04(context: Context) : Day by context {
+    data class Board(val turn: Int, val score: Int)
+
+    val chunks = input.split(EMPTY_LINE).map { it.allInts().toList() }
+    val numbers = chunks.first()
+    val numberToTurn = numbers.withIndex().associate { (i, n) -> n to i }
+    val turnToNumber = numbers.withIndex().associate { (i, n) -> i to n }
+    val boards = chunks.drop(1).map { b ->
+        with(Grid(5, 5, b)) {
+            val turn = (rowsValues + columnsValues).minOf { it.maxOf(numberToTurn::getValue) }
+            val unmarked = filter { numberToTurn.getValue(it) > turn }.sum()
+            val number = turnToNumber.getValue(turn)
+            Board(turn, unmarked * number)
+        }
+    }
+
+    fun part1() = boards.minBy { it.turn }.score
+    fun part2() = boards.maxBy { it.turn }.score
+}
---
+++ year2021/src/test/kotlin/year2021/Day04Test.kt
@@ -0,0 +1,15 @@
+package year2021
+
+import framework.Test
+
+class Day04Test : Test({
+    part1 {
+        example = 4512
+        input = 41503
+    }
+
+    part2 {
+        example = 1924
+        input = 3178
+    }
+})
