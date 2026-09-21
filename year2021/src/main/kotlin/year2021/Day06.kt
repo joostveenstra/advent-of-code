ee8452f72b5b45c3f87979a25870c959536f0e59
Joost Veenstra <Joost.Veenstra@keylane.com>
2026-09-21 12:07:27 +0200

    Day 6: Lanternfish

---
+++ year2021/src/main/kotlin/year2021/Day06.kt
@@ -0,0 +1,17 @@
+package year2021
+
+import framework.Context
+import framework.Day
+import util.allInts
+
+class Day06(context: Context) : Day by context {
+    val fish = input.allInts()
+
+    fun simulate(days: Int) = LongArray(9).apply {
+        fish.forEach { i -> this[i] += 1 }
+        (0..<days).forEach { day -> this[(day + 7) % 9] += this[day % 9] }
+    }.sum()
+
+    fun part1() = simulate(80)
+    fun part2() = simulate(256)
+}
---
+++ year2021/src/test/kotlin/year2021/Day06Test.kt
@@ -0,0 +1,15 @@
+package year2021
+
+import framework.Test
+
+class Day06Test : Test({
+    part1 {
+        example = 5934
+        input = 360761
+    }
+
+    part2 {
+        example = 26984457539
+        input = 1632779838045
+    }
+})
