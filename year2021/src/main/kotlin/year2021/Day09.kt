c67016f93d764bf9c1cd9804feba4fb8bcfbd137
Joost Veenstra <Joost.Veenstra@keylane.com>
2026-09-22 15:47:10 +0200

    Day 9: Smoke Basin

---
+++ year2021/src/main/kotlin/year2021/Day09.kt
@@ -0,0 +1,32 @@
+package year2021
+
+import framework.Context
+import framework.Day
+import util.*
+
+class Day09(context: Context) : Day by context {
+    val grid = input.toDigitGrid()
+    val lowPoints = grid.findLowPoints()
+
+    fun IntGrid.findLowPoints() = points.filter { p ->
+        val height = get(p)
+        p.cardinalElements().all { it > height }
+    }
+
+    fun IntGrid.getBasinSize(position: Point): Int {
+        val queue = dequeOf(position)
+        val visited = mutableSetOf(position)
+
+        queue.drain { p ->
+            p.cardinal().filter { it !in visited }.filter { get(it) < 9 }.forEach { next ->
+                visited.add(next)
+                queue.add(next)
+            }
+        }
+
+        return visited.size
+    }
+
+    fun part1() = lowPoints.sumOf { grid[it] + 1 }
+    fun part2() = lowPoints.map { grid.getBasinSize(it) }.sorted().takeLast(3).product()
+}
---
+++ year2021/src/test/kotlin/year2021/Day09Test.kt
@@ -0,0 +1,15 @@
+package year2021
+
+import framework.Test
+
+class Day09Test : Test({
+    part1 {
+        example = 15
+        input = 564
+    }
+
+    part2 {
+        example = 1134
+        input = 1038240
+    }
+})
