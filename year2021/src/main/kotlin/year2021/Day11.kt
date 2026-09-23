640d6994c74b00b14aacecacb6959788cc9cbfe6
Joost Veenstra <Joost.Veenstra@keylane.com>
2026-09-23 22:16:12 +0200

    Day 11: Dumbo Octopus

---
+++ year2021/src/main/kotlin/year2021/Day11.kt
@@ -0,0 +1,39 @@
+package year2021
+
+import framework.Context
+import framework.Day
+import util.*
+
+class Day11(context: Context) : Day by context {
+    val initial = input.toDigitGrid()
+
+    fun IntGrid.simulate() = sequence {
+        val cave = initial.toMutableGrid()
+        val queue = dequeOf<Point>()
+        
+        fun increaseEnergy(position: Point) {
+            if (cave[position] < 9) {
+                cave[position] += 1
+            } else {
+                queue.add(position)
+                cave[position] = 0
+            }
+        }
+        
+        while (true) {
+            points.forEach(::increaseEnergy)
+
+            queue.drain { position ->
+                position
+                    .allAdjacent()
+                    .filter { cave[it] != 0 }
+                    .forEach(::increaseEnergy)
+            }
+
+            yield(cave.count { it == 0 })
+        }
+    }
+
+    fun part1() = initial.simulate().take(100).sum()
+    fun part2() = initial.simulate().indexOfFirst { it == 100 } + 1
+}
---
+++ year2021/src/test/kotlin/year2021/Day11Test.kt
@@ -0,0 +1,15 @@
+package year2021
+
+import framework.Test
+
+class Day11Test : Test({
+    part1 {
+        example = 1656
+        input = 1649
+    }
+
+    part2 {
+        example = 195
+        input = 256
+    }
+})
